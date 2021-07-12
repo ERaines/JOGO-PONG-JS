7726b6ae5491a4a1a014deb989432082d09d46e9
Edson V. Ferreira <74566709+ERaines@users.noreply.github.com>
2021-07-12 08:54:01 -0300

    organizando files

--- script.js
+++ script.js
@@ -32,9 +32,9 @@ let velocidadeYOponente;
 let chanceDeErrar = 0;
 
 function preload(){
-  trilha = loadSound("trilha.mp3");
-  ponto = loadSound("ponto.mp3");
-  raquetada = loadSound("raquetada.mp3");
+  trilha = loadSound("sound/trilha.mp3");
+  ponto = loadSound("sound/ponto.mp3");
+  raquetada = loadSound("sound/raquetada.mp3");
 }
 
 function setup() {
